f01fa4e172c9ea760a57b9082fcc963181e2cd80
XunJang <xunjang@outlook.com>
2026-05-23 09:43:01 +0800

    Support MinGW libxmp-lite import library names

--- ports/libxmp-lite/vcpkg-cmake-wrapper.cmake
+++ ports/libxmp-lite/vcpkg-cmake-wrapper.cmake
@@ -1,27 +1,55 @@
 set(z_vcpkg_libxmp_lite_root "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}")
 set(z_vcpkg_libxmp_lite_include_dir "${z_vcpkg_libxmp_lite_root}/include/libxmp-lite")
+set(z_vcpkg_libxmp_lite_shared_implib_release "")
+set(z_vcpkg_libxmp_lite_shared_implib_debug "")
+set(z_vcpkg_libxmp_lite_static_lib_release "")
+set(z_vcpkg_libxmp_lite_static_lib_debug "")
+
+if(EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.lib")
+    set(z_vcpkg_libxmp_lite_shared_implib_release "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.lib")
+elseif(EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.dll.a")
+    set(z_vcpkg_libxmp_lite_shared_implib_release "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.dll.a")
+endif()
+
+if(EXISTS "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.lib")
+    set(z_vcpkg_libxmp_lite_shared_implib_debug "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.lib")
+elseif(EXISTS "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.dll.a")
+    set(z_vcpkg_libxmp_lite_shared_implib_debug "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.dll.a")
+endif()
+
+if(EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite-static.lib")
+    set(z_vcpkg_libxmp_lite_static_lib_release "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite-static.lib")
+elseif(EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.a")
+    set(z_vcpkg_libxmp_lite_static_lib_release "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.a")
+endif()
+
+if(EXISTS "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite-static.lib")
+    set(z_vcpkg_libxmp_lite_static_lib_debug "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite-static.lib")
+elseif(EXISTS "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.a")
+    set(z_vcpkg_libxmp_lite_static_lib_debug "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.a")
+endif()
 
 if(NOT TARGET "libxmp-lite::xmp_lite_shared"
-        AND EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.lib"
+        AND NOT z_vcpkg_libxmp_lite_shared_implib_release STREQUAL ""
         AND EXISTS "${z_vcpkg_libxmp_lite_root}/bin/libxmp-lite.dll")
     add_library("libxmp-lite::xmp_lite_shared" SHARED IMPORTED)
     set_target_properties("libxmp-lite::xmp_lite_shared" PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_libxmp_lite_include_dir}"
-        IMPORTED_IMPLIB_RELEASE "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite.lib"
+        IMPORTED_IMPLIB_RELEASE "${z_vcpkg_libxmp_lite_shared_implib_release}"
         IMPORTED_LOCATION_RELEASE "${z_vcpkg_libxmp_lite_root}/bin/libxmp-lite.dll"
-        IMPORTED_IMPLIB_DEBUG "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite.lib"
+        IMPORTED_IMPLIB_DEBUG "${z_vcpkg_libxmp_lite_shared_implib_debug}"
         IMPORTED_LOCATION_DEBUG "${z_vcpkg_libxmp_lite_root}/debug/bin/libxmp-lite.dll"
     )
 endif()
 
 if(NOT TARGET "libxmp-lite::xmp_lite_static"
-        AND EXISTS "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite-static.lib")
+        AND NOT z_vcpkg_libxmp_lite_static_lib_release STREQUAL "")
     add_library("libxmp-lite::xmp_lite_static" STATIC IMPORTED)
     set_target_properties("libxmp-lite::xmp_lite_static" PROPERTIES
         INTERFACE_COMPILE_DEFINITIONS "LIBXMP_STATIC"
         INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_libxmp_lite_include_dir}"
-        IMPORTED_LOCATION_RELEASE "${z_vcpkg_libxmp_lite_root}/lib/libxmp-lite-static.lib"
-        IMPORTED_LOCATION_DEBUG "${z_vcpkg_libxmp_lite_root}/debug/lib/libxmp-lite-static.lib"
+        IMPORTED_LOCATION_RELEASE "${z_vcpkg_libxmp_lite_static_lib_release}"
+        IMPORTED_LOCATION_DEBUG "${z_vcpkg_libxmp_lite_static_lib_debug}"
     )
 endif()
 
@@ -45,3 +73,7 @@ endif()
 
 unset(z_vcpkg_libxmp_lite_root)
 unset(z_vcpkg_libxmp_lite_include_dir)
+unset(z_vcpkg_libxmp_lite_shared_implib_release)
+unset(z_vcpkg_libxmp_lite_shared_implib_debug)
+unset(z_vcpkg_libxmp_lite_static_lib_release)
+unset(z_vcpkg_libxmp_lite_static_lib_debug)
